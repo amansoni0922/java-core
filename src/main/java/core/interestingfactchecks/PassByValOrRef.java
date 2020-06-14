9afd66a9d9b1b57dba178dbad210d0af34316191
amansoni0922 <amansoni0922@gmail.com>
2020-06-14 22:47:56 +0530

    minor

--- src/main/java/core/interestingfactchecks/PassByValOrRef.java
+++ src/main/java/core/interestingfactchecks/PassByValOrRef.java
@@ -3,6 +3,11 @@ package core.interestingfactchecks;
 public class PassByValOrRef {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Primitives are pass by value whereas objects are pass by reference.
+		 */
+		
 		// TODO Auto-generated method stub
 		// www.google.com/search?q=is+java+pass+by+value+or+pass+by+reference&oq=is+java+pass+by+value+or+pass+by+reference
 		// https://www.javaworld.com/article/3512039/does-java-pass-by-reference-or-pass-by-value.html
